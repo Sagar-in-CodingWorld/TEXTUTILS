19d2d50064b4f17475b1e60ee086e1976c2cc466
sagar <sagarascoder@gmail.com>
2024-07-17 01:18:43 +0530

    Initial Commit on 17-07-2024 at 01:18:31

---
+++ src/App.js
@@ -0,0 +1,35 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <>
+   <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">TEXTUTILS</a> 
+   
+   
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">About</a>
+        </li>
+        
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+   
+   
+   </>
+  );
+}
+
+export default App;
